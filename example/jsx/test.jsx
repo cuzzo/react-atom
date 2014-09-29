b081fbc44b3c31d09d635a3d8cc6a0236a039349
Cuzzo <yahn007@outlook.com>
2014-09-29 10:18:43 -0700

    Added README and example. Used role for BEM element.

---
+++ example/jsx/test.jsx
@@ -0,0 +1,33 @@
+/** @jsx React.DOM */
+var Test = React.createClass({
+  mixins: [ReactBEM, ReactATOM],
+
+  bem_blocks: ["widget"],
+
+  bem_translate_class: function(bem_classes) {
+    var resp = this.atomize(bem_classes);
+    console.log("TRANS", bem_classes, resp);
+    return resp;
+  },
+
+  bem_render: function() {
+    return (
+      <section class="no-overwrite">
+        <h3 role="title">Price Form</h3>
+        <form>
+          <input type="text" name="amount" modifiers="amount" />
+          <input type="submit" name="submit" modifiers="submit" role="button" />
+        </form>
+      </section>
+    );
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+
+  React.renderComponent(
+    <Test />,
+    document.getElementsByTagName("body")[0]
+  );
+
+});
